eaa5ed7dbae9f16cc9df09ddc86dda8fc1d28cb5
rockan007 <rockan007@gmail.com>
2019-08-22 11:54:08 +0800

    service part 0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 
 
@@ -9,6 +10,8 @@ const routes: Routes = [{
   path: '', component: ProductListComponent
 }, {
   path: 'products/:productId', component: ProductDetailsComponent
+}, {
+  path: 'cart', component: CartComponent
 }];
 
 @NgModule({
